af6d498d3d838b7aa142d088f1abad11cc2bcb81
PETER MAKWAKWA <MAKWAKWAPETER@outlook.com>
2019-01-10 12:44:29 +0200

    Add project files.

---
+++ easyPrimeNumbers/PrimeNumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace easyPrimeNumbers
+{
+    public class PrimeNumbers : IPrimeNumbers
+    {
+        public void PrimeNumer(int range)
+        {
+            int count = 0;
+            //List<int> listPrimeNumber = new List<int>();
+
+            for (int i = 1; i <= range; i++)
+            {
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j != 0)
+                    {
+                        count += 1;
+                    }
+                }
+                if (count == (i - 2))
+                {
+                    //listPrimeNumber.Add(count);
+                    Console.Write(i + "\t");
+                }
+
+                count = 0;
+            }
+            //return listPrimeNumber;
+        }
+    }
+}
---
+++ easyPrimeNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace easyPrimeNumbers
+{
+    class Program
+    {
+        private readonly IPrimeNumbers _primeNumbers;
+
+        public Program(IPrimeNumbers primeNumbers)
+        {
+            _primeNumbers = primeNumbers;
+        }
+
+        static void Main(string[] args)
+        {
+            Program pp = new Program(new PrimeNumbers());
+            pp._primeNumbers.PrimeNumer(100);
+            Console.ReadKey();
+        }
+    }
+}
